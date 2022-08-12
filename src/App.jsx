52814bfbed013c6eaa77504172f1bc0d25823312
Adal Obregon <adalobregoon@gmail.com>
2022-08-11 21:21:20 -0500

    Se agrego el  componente listado de pacientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Form from "./components/Form";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   return (
     <>
       <Form />
+      <ListadoPacientes />
     </>
   );
 }
