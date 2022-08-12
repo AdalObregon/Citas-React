d6da55e4abc2520a7125c05ae6a2bf660398a30c
Adal Obregon <adalobregoon@gmail.com>
2022-08-11 21:11:52 -0500

    Form first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React from "react";
+import Form from "./components/Form";
 
 function App() {
-  const edad = 18;
-
   return (
     <>
-      <div className='App'>
-        <h1>Hola Mundo</h1>
-        <p>Un Parrafo</p>
-      </div>
-      <div className='App'>
-        <h1>Hola Mundo</h1>
-        <p>Un Parrafo</p>
-      </div>
+      <Form />
     </>
   );
 }
