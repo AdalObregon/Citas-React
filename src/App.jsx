b648a39da5c1c95be814ccf29ae68d3687aeb6c6
Adal Obregon <adalobregoon@gmail.com>
2022-08-11 21:39:35 -0500

    fix conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
-<<<<<<< HEAD
       <div className='App'>
         <Header />
       </div>
-=======
-
->>>>>>> main
     </>
   );
 }
